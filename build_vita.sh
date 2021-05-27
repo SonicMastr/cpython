8fb2a0e1356f83d80b33be691923a47c015f2f77
SonicMastr <jaylongowie46@gmail.com>
2021-05-26 21:00:54 -0500

    Fix the prefix

--- build_vita.sh
+++ build_vita.sh
@@ -6,6 +6,6 @@ if [ -z "$1" ]
     else
         export PREFIX=$1
 fi
-./configure --build=armv7 --host=arm-vita-eabi --prefix="$VITASDK/arm-vita-eabi" --enable-optimizations --disable-shared --with-threads --disable-ipv6 --without-signal-module CONFIG_SITE="config.site" CFLAGS="-Wl,-q,-z,nocopyreloc" CPPFLAGS="-Wl,-q,-z,nocopyreloc" LDFLAGS="-Wl,--allow-multiple-definition"
+./configure --build=armv7 --host=arm-vita-eabi --prefix="$PREFIX" --enable-optimizations --disable-shared --with-threads --disable-ipv6 --without-signal-module CONFIG_SITE="config.site" CFLAGS="-Wl,-q,-z,nocopyreloc" CPPFLAGS="-Wl,-q,-z,nocopyreloc" LDFLAGS="-Wl,--allow-multiple-definition"
 make clean
 make -j$(nproc) install
